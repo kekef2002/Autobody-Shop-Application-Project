f9e80a77521d6b944052705268ecee00ca3ab80c
yingxingxin <ssem1722@gmail.com>
2024-11-25 18:10:44 -0500

    splashscreen in main

---
+++ src/main/java/edu/farmingdale/autobodyshopapplicationproject/MainApplication.java
@@ -0,0 +1,45 @@
+package edu.farmingdale.autobodyshopapplicationproject;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader splashLoader = new FXMLLoader(MainApplication.class.getResource("splashscreen-page.fxml"));
+        Pane splashPane = splashLoader.load();
+
+        Scene splashScene = new Scene(splashPane, 1440, 1024);
+        stage.setTitle("AutoBody Shop");
+        stage.setScene(splashScene);
+        stage.show();
+
+        // Apply Fade Transition
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), splashPane);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.0);
+        fadeTransition.setOnFinished(event -> {
+            try {
+                // Load Home Page
+                FXMLLoader homeLoader = new FXMLLoader(MainApplication.class.getResource("home-page.fxml"));
+                Scene homeScene = new Scene(homeLoader.load(), 1440, 1024);
+                stage.setScene(homeScene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        fadeTransition.play();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
