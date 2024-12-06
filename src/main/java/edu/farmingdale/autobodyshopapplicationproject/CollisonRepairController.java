3e4c95f5cce3959ae7cd976fdce7beca90fdb6ff
maradC <maradiagachristopher5@gmail.com>
2024-12-06 13:46:36 -0500

    Created the Collison controller

---
+++ src/main/java/edu/farmingdale/autobodyshopapplicationproject/CollisonRepairController.java
@@ -0,0 +1,18 @@
+package edu.farmingdale.autobodyshopapplicationproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class CollisonRepairController {
+    @FXML
+    private ImageView v14_48;
+
+    @FXML
+    private ImageView v208_15;
+
+    @FXML
+    private ImageView v87_5;
+
+
+}
+
