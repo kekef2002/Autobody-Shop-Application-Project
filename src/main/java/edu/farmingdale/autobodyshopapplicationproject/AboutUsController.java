200c1d7e27aea0b704fd0315b7050205f0213bf5
maradC <maradiagachristopher5@gmail.com>
2024-12-06 13:44:37 -0500

    Created the fxml controller

---
+++ src/main/java/edu/farmingdale/autobodyshopapplicationproject/AboutUsController.java
@@ -0,0 +1,16 @@
+package edu.farmingdale.autobodyshopapplicationproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class AboutUsController {
+    @FXML
+    private ImageView ServiceImageView;
+
+    @FXML
+    private ImageView aboutImageView;
+
+    @FXML
+    private ImageView v15_14;
+
+}
