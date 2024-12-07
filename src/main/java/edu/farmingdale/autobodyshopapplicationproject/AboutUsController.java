c50a168f2aad4624c42a20471330553c77896183
Kervin Francois <mrkary2010@gmail.com>
2024-12-07 05:29:20 -0500

    Modified the file for correctness

--- src/main/java/edu/farmingdale/autobodyshopapplicationproject/AboutUsController.java
+++ src/main/java/edu/farmingdale/autobodyshopapplicationproject/AboutUsController.java
@@ -1,9 +1,13 @@
 package edu.farmingdale.autobodyshopapplicationproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 
 public class AboutUsController {
+
     @FXML
     private ImageView ServiceImageView;
 
@@ -11,6 +15,78 @@ public class AboutUsController {
     private ImageView aboutImageView;
 
     @FXML
-    private ImageView v15_14;
+    private Button about_us_button;
+
+    @FXML
+    private MenuItem auto_glass_replacement;
+
+    @FXML
+    private MenuItem auto_repairs_maintenance;
+
+    @FXML
+    private MenuItem celd;
+
+    @FXML
+    private MenuItem collision_repairs;
+
+    @FXML
+    private Button gallery_button;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button home_login_button;
+
+    @FXML
+    private Button schedule_an_appointment_button;
+
+    @FXML
+    private ImageView v15_147;
+
+    @FXML
+    void onAboutUsButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onApptointmentButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAutoGlassReplacementClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAutoRepairsMaintenanceClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCELDClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCollisionRepairsClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGalleryButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHomeButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHomeLoginButtonClick(ActionEvent event) {
+
+    }
 
 }
